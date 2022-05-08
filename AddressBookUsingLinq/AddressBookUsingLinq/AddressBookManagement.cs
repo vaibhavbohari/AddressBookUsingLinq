a156812d2cc0f5363677987036aa3dc3891c4f19
vaibhavbohari <vaibhavbohari@gmail.com>
2022-05-08 14:47:46 +0530

    Inserting Values in data table

--- AddressBookUsingLinq/AddressBookUsingLinq/AddressBookManagement.cs
+++ AddressBookUsingLinq/AddressBookUsingLinq/AddressBookManagement.cs
@@ -10,7 +10,7 @@ namespace AddressBookUsingLinq
     public class AddressBookManagement
     {
         public DataTable dataTable = new DataTable();
-        public AddressBookManagement()
+        public  AddressBookManagement()
         {
             dataTable.Columns.Add("FirstName", typeof(string));
             dataTable.Columns.Add("LastName", typeof(string));
@@ -20,6 +20,14 @@ namespace AddressBookUsingLinq
             dataTable.Columns.Add("Zip", typeof(double));
             dataTable.Columns.Add("PhoneNumber", typeof(double));
             dataTable.Columns.Add("Email", typeof(string));
+
+            // Creating rows and adding data into row
+            dataTable.Rows.Add("Vaibhav", "Bohari", "Agar", "Akola", "Maharashtra", 444006, 9730913415, "vaibhav@gmail.com");
+            dataTable.Rows.Add("sumit", "Patel", "GandhiNagar", "Amravati", "Goa", 509000, 9785474102, "Sumit@gmail.com");
+            dataTable.Rows.Add("Shubham", "Kulkarni", "Mumbai", "RamNagar", "Maharashtra", 409211, 8415789858, "shubham@gmail.com");
+            dataTable.Rows.Add("Rohit", "Fuse", "janmod", "Buldhana", "mp", 489201, 9458257321, "Rohit@gmail.com");
+            dataTable.Rows.Add("Gopal", "Wagh", "Api", "Pune", "Maharashtra", 478952, 841789456, "Gopal@gmail.com");
+            dataTable.Rows.Add("Vinavak", "Kute", "GandhiRoad", "Hadapser", "mp", 489206, 7894567112, "vinavak@gmail.com");
         }
     }
 }
