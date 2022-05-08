788a0f1bb1054012e3c4084f2b432d511365f4c8
vaibhavbohari <vaibhavbohari@gmail.com>
2022-05-08 14:58:57 +0530

    Update Value

--- AddressBookUsingLinq/AddressBookUsingLinq/AddressBookManagement.cs
+++ AddressBookUsingLinq/AddressBookUsingLinq/AddressBookManagement.cs
@@ -29,7 +29,39 @@ namespace AddressBookUsingLinq
             dataTable.Rows.Add("Gopal", "Wagh", "Api", "Pune", "Maharashtra", 478952, 841789456, "Gopal@gmail.com");
             dataTable.Rows.Add("Vinavak", "Kute", "GandhiRoad", "Hadapser", "mp", 489206, 7894567112, "vinavak@gmail.com");
         }
+
+        // Displays the table contents.
+        public void getAllData()
+        {
+            foreach (var data in dataTable.AsEnumerable())
+            {
+                Console.WriteLine("FirstName: " + data.Field<string>("Firstname") + ","
+                    + "LastName: " + data.Field<string>("LastName") + ","
+                    + "Address: " + data.Field<string>("Address") + ","
+                    + "City: " + data.Field<string>("City") + ","
+                    + "State: " + data.Field<string>("State") + ","
+                    + "Zip: " + data.Field<double>("Zip") + ","
+                    + "PhoneNumber: " + data.Field<double>("PhoneNumber") + ","
+                    + "Email: " + data.Field<string>("Email"));
+                Console.WriteLine("\n");
+            }
+        }
+        public void UpdatePersonByName()
+        {
+            Console.WriteLine("Enter FirstName : ");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter columnName : ");
+            string columnName = Console.ReadLine();
+            Console.WriteLine("Enter Upadated Value : ");
+            string Updatedvalue = Console.ReadLine();
+            DataRow updatedperson = dataTable.Select("FirstName = '" + firstName + "'").FirstOrDefault();
+            updatedperson[columnName] = Updatedvalue;
+            Console.WriteLine("Contanted is Updated ");
+            getAllData();
+        }
     }
 }
+    
+
 
 
--- AddressBookUsingLinq/AddressBookUsingLinq/Program.cs
+++ AddressBookUsingLinq/AddressBookUsingLinq/Program.cs
@@ -7,6 +7,38 @@ namespace AddressBookUsingLinq
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to AddressBook Using Linq");
+            AddressBookManagement management = new AddressBookManagement();
+            bool i = true;
+            while (i)
+            {
+                Console.WriteLine("1.GetAllRecords");
+                Console.WriteLine("2.Update The person ");
+                Console.WriteLine("3.Exit");
+                try
+                {
+                    int choice = Convert.ToInt32(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            management.getAllData();
+                            break;
+                        case 2:
+                            management.UpdatePersonByName();
+                            break;
+                        case 3:
+                            i = false;
+                            break;
+                        default:
+                            Console.WriteLine("Choose proper option");
+                            break;
+                    }
+                }
+                catch (System.FormatException formatException)
+                {
+                    Console.WriteLine(formatException);
+                }
+            }
         }
     }
 }
+    
